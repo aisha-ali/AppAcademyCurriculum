d1affc1dc04bca5fd2dcbd0519ccef1aeceb1167
aisha-ali <aali.aishaali@gmail.com>
2021-01-15 15:32:52 -0500

    JS Looping Constructs

--- javascript/IntroJSProblems/introjs.js
+++ javascript/IntroJSProblems/introjs.js
@@ -7,8 +7,6 @@ function mysteryScoping1() {
   console.log(x);
 }
 
-mysteryScoping1(); // in block, in block
-
 function mysteryScoping2() {
   const x = 'out of block';
   if (true) {
@@ -17,8 +15,6 @@ function mysteryScoping2() {
   }
   console.log(x);
 }
-
-mysteryScoping2(); // in block, out of block
 
 function mysteryScoping3() {
   const x = 'out of block';
@@ -29,8 +25,6 @@ function mysteryScoping3() {
   console.log(x);
 }
 
-mysteryScoping3(); // SyntaxError
-
 function mysteryScoping4() {
   let x = 'out of block';
   if (true) {
@@ -39,8 +33,6 @@ function mysteryScoping4() {
   }
   console.log(x);
 }
-
-mysteryScoping4(); // in block, out of block
 
 function mysteryScoping5() {
   let x = 'out of block';
@@ -52,5 +44,54 @@ function mysteryScoping5() {
   console.log(x);
 }
 
-mysteryScoping5(); // SyntaxError
+function madLib(verb, adj, noun) {
+  return `We shall ${verb.toUpperCase()} the ${adj.toUpperCase()} ${noun.toUpperCase()}.`
+}
 
+function isSubstring(searchString, subString) {
+  return searchString.includes(subString);
+}
+
+function fizzBuzz(array) {
+  const fizzBuzzarray = [];
+
+  array.forEach(el => {
+    if ((el % 3 === 0) || (el % 5 === 0)) {
+      fizzBuzzarray.push(el);
+    }
+  });
+  return fizzBuzzarray;
+}
+
+console.log(fizzBuzz([1, 2, 3, 4, 5]));
+
+function isPrime(n) {
+  if (n < 2) { return false; }
+
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(isPrime(13));
+
+function sumOfNPrimes(n) {
+  let sum = 0;
+  let countPrimes = 0;
+  let i = 2;
+
+  while (countPrimes < n) {
+    if (isPrime(i)) {
+      sum += i;
+      countPrimes++;
+    }
+    i++;
+  }
+  return sum;
+}
+
+console.log(sumOfNPrimes(1));
