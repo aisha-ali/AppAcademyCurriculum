fcfe7f4a85d6f8d21244da8fa4eb384c1a78d9bf
aisha-ali <aali.aishaali@gmail.com>
2021-01-18 20:39:52 -0500

    timeout and diff ways to call a fcn

---
+++ javascript/Callbacks and Function Calls/simpletimeout.js
@@ -0,0 +1,2 @@
+window.setTimeout(function(){alert('HAMMER TIME!');},5000);function hammerTime(time){window.setTimeout(function(){alert(`${time} is HAMMER TIME!`);});}
+// Open Chrome Devtools and paste code into the console to test it
