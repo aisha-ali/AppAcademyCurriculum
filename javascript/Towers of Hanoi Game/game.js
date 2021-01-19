e9270ae6a8bab3ab41cc0eda0cd4845ce2647100
aisha-ali <aali.aishaali@gmail.com>
2021-01-18 19:15:22 -0500

    Playing the game

--- javascript/Towers of Hanoi Game/game.js
+++ javascript/Towers of Hanoi Game/game.js
@@ -6,8 +6,8 @@ class Game {
   isValidMove(startTowerIdx, endTowerIdx) {
     const startTower = this.towers[startTowerIdx];
     const endTower = this.towers[endTowerIdx];
-
-    if (startTower.length === 0) {
+    
+    if (startTower.length == 0) {
       return false;
     } else if (endTower.length == 0) {
       return true;
