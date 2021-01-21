b75f4389eadc2b00a51cdc6bae10a3c61641a5e8
aisha-ali <aali.aishaali@gmail.com>
2021-01-21 18:13:21 -0500

    drawing a rectangle

--- javascript/canvas_practice/practice.js
+++ javascript/canvas_practice/practice.js
@@ -1,3 +1,10 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
+  const canvasEl = document.getElementById("mycanvas");
+  canvasEl.width = 500;
+  canvasEl.height = 500;
+
+  const ctx = canvasEl.getContext("2d");
+  ctx.fillStyle = "pink";
+  ctx.fillRect(100, 100, 300, 300);
 
 });
