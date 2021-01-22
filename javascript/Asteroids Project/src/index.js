31ff0dd6e82f6c3eea8323aa20c07626cd568df2
aisha-ali <aali.aishaali@gmail.com>
2021-01-22 16:06:33 -0500

    moving object class

--- javascript/Asteroids Project/src/index.js
+++ javascript/Asteroids Project/src/index.js
@@ -1,1 +1,15 @@
+const MovingObject = require("./moving_object.js");
+window.MovingObject = MovingObject;
+
+document.addEventListener("DOMContentLoaded", function () {
+  const canvasEl = document.getElementsByTagName("canvas")[0];
+  canvasEl.width = Game.DIM_X;
+  canvasEl.height = Game.DIM_Y;
+
+  const ctx = canvasEl.getContext("2d");
+  const game = new Game();
+  new GameView(game, ctx).start();
+});
+
+
 console.log("Webpack is working!")
