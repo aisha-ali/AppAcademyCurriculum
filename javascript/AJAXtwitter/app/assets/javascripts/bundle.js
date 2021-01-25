39a9ff4cfc4e78a4d467475ca8248139b443fa79
aisha-ali <aali.aishaali@gmail.com>
2021-01-25 17:39:53 -0500

    users search

--- javascript/AJAXtwitter/app/assets/javascripts/bundle.js
+++ javascript/AJAXtwitter/app/assets/javascripts/bundle.js
@@ -20,6 +20,33 @@ const APIUtil = {
       method
     })
   ),
+
+  searchUsers: query => (
+    $.ajax({
+      url: '/users/search',
+      dataType: 'json',
+      method: 'GET',
+      data: { query }
+    })
+  ),
+
+  createTweet: data => (
+    $.ajax({
+      url: '/tweets',
+      method: 'POST',
+      dataType: 'json',
+      data
+    })
+  ),
+
+  fetchTweets: data => (
+    $.ajax({
+      url: '/feed',
+      method: 'GET',
+      dataType: 'json',
+      data
+    })
+  )
 };
 
 module.exports = APIUtil;
@@ -94,6 +121,63 @@ module.exports = FollowToggle;
 
 
 
+/***/ }),
+
+/***/ "./frontend/users_search.js":
+/*!**********************************!*\
+  !*** ./frontend/users_search.js ***!
+  \**********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+const APIUtil = __webpack_require__(/*! ./api_util */ "./frontend/api_util.js");
+const FollowToggle = __webpack_require__(/*! ./follow_toggle */ "./frontend/follow_toggle.js");
+
+class UsersSearch {
+  constructor(el) {
+    this.$el = $(el);
+    this.$input = this.$el.find('input[name=username]');
+    this.$ul = this.$el.find('.users');
+
+    this.$input.on('input', this.handleInput.bind(this));
+  }
+
+  handleInput(event) {
+    if (this.$input.val() === '') {
+      this.renderResults([]);
+      return;
+    }
+    APIUtil.searchUsers(this.$input.val())
+      .then(users => this.renderResults(users));
+  }
+
+  renderResults(users) {
+    this.$ul.empty();
+
+    for (let i = 0; i < users.length; i++) {
+      const user = users[i];
+
+      let $a = $('<a></a>');
+      $a.text(`@${user.username}`);
+      $a.attr('href', `/users/${user.id}`);
+
+      const $followToggle = $('<button></button>');
+      new FollowToggle($followToggle, {
+        userId: user.id,
+        followState: user.followed ? 'followed' : 'unfollowed'
+      });
+
+      const $li = $('<li></li>');
+      $li.append($a);
+      $li.append($followToggle);
+
+      this.$ul.append($li);
+    }
+  }
+}
+
+module.exports = UsersSearch;
+
+
 /***/ })
 
 /******/ 	});
@@ -129,7 +213,7 @@ module.exports = FollowToggle;
 const FollowToggle = __webpack_require__(/*! ./follow_toggle */ "./frontend/follow_toggle.js");
 const InfiniteTweets = __webpack_require__(Object(function webpackMissingModule() { var e = new Error("Cannot find module './infinite_tweets'"); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
 const TweetCompose = __webpack_require__(Object(function webpackMissingModule() { var e = new Error("Cannot find module './tweet_compose'"); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
-const UsersSearch = __webpack_require__(Object(function webpackMissingModule() { var e = new Error("Cannot find module './users_search'"); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+const UsersSearch = __webpack_require__(/*! ./users_search */ "./frontend/users_search.js");
 
 $(function () {
   $('div.infinite-tweets').each((i, tweet) => new InfiniteTweets(tweet));
