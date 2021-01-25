57e050b9f8174827f2c842dfc68fa54f4433b9a5
aisha-ali <aali.aishaali@gmail.com>
2021-01-25 16:49:57 -0500

    FollowToggle class and API Util

--- javascript/AJAXtwitter/app/assets/javascripts/bundle.js
+++ javascript/AJAXtwitter/app/assets/javascripts/bundle.js
@@ -1,7 +1,146 @@
 /******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./frontend/api_util.js":
+/*!******************************!*\
+  !*** ./frontend/api_util.js ***!
+  \******************************/
+/***/ ((module) => {
+
+const APIUtil = {
+
+  followUser: id => APIUtil.changeFollowStatus(id, 'POST'),
+
+  unfollowUser: id => APIUtil.changeFollowStatus(id, 'DELETE'),
+
+  changeFollowStatus: (id, method) => (
+    $.ajax({
+      url: `/users/${id}/follow`,
+      dataType: 'json',
+      method
+    })
+  ),
+};
+
+module.exports = APIUtil;
+
+
+/***/ }),
+
+/***/ "./frontend/follow_toggle.js":
+/*!***********************************!*\
+  !*** ./frontend/follow_toggle.js ***!
+  \***********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+const APIUtil = __webpack_require__(/*! ./api_util */ "./frontend/api_util.js");
+
+class FollowToggle {
+  constructor(el, options) {
+    this.$el = $(el);
+    this.userId = this.$el.data('user-id') || options.userId;
+    this.followState = (this.$el.data('initial-follow-state') ||
+      options.followState);
+    this.render();
+
+    this.$el.on('click', this.handleClick.bind(this));
+  }
+
+  handleClick(event) {
+    const followToggle = this;
+
+    event.preventDefault();
+
+    if (this.followState === 'followed') {
+      this.followState = 'unfollowing';
+      this.render();
+      APIUtil.unfollowUser(this.userId).then(() => {
+        followToggle.followState = 'unfollowed';
+        followToggle.render();
+      });
+    } else if (this.followState === 'unfollowed') {
+      this.followState = 'following';
+      this.render();
+      APIUtil.followUser(this.userId).then(() => {
+        followToggle.followState = 'followed';
+        followToggle.render();
+      });
+    }
+  }
+
+  render() {
+    switch (this.followState) {
+      case 'followed':
+        this.$el.prop('disabled', false);
+        this.$el.html('Unfollow!');
+        break;
+      case 'unfollowed':
+        this.$el.prop('disabled', false);
+        this.$el.html('Follow!');
+        break;
+      case 'following':
+        this.$el.prop('disabled', true);
+        this.$el.html('Following...');
+        break;
+      case 'unfollowing':
+        this.$el.prop('disabled', true);
+        this.$el.html('Unfollowing...');
+        break;
+    }
+  }
+}
+
+module.exports = FollowToggle;
+
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		if(__webpack_module_cache__[moduleId]) {
+/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+(() => {
 /*!*****************************!*\
   !*** ./frontend/twitter.js ***!
   \*****************************/
+const FollowToggle = __webpack_require__(/*! ./follow_toggle */ "./frontend/follow_toggle.js");
+const InfiniteTweets = __webpack_require__(Object(function webpackMissingModule() { var e = new Error("Cannot find module './infinite_tweets'"); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+const TweetCompose = __webpack_require__(Object(function webpackMissingModule() { var e = new Error("Cannot find module './tweet_compose'"); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+const UsersSearch = __webpack_require__(Object(function webpackMissingModule() { var e = new Error("Cannot find module './users_search'"); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+
+$(function () {
+  $('div.infinite-tweets').each((i, tweet) => new InfiniteTweets(tweet));
+  $('form.tweet-compose').each((i, form) => new TweetCompose(form));
+  $('.users-search').each((i, search) => new UsersSearch(search));
+  $('button.follow-toggle').each((i, btn) => new FollowToggle(btn, {}));
+});
+
+
+})();
 
 /******/ })()
 ;
+//# sourceMappingURL=bundle.js.map
