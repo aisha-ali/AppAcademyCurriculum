46a3da55deb0b71f2daff1e8d2a005c08883cc23
aisha-ali <aali.aishaali@gmail.com>
2021-01-29 18:03:59 -0500

    tabs components

--- React/widgets/frontend/widgets.jsx
+++ React/widgets/frontend/widgets.jsx
@@ -2,14 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Clock from './clock';
+import Tabs from './tabs';
+
+const names = [
+  'Aisha',
+  'Ali'
+];
+
+const panes = [
+  { title: 'one', content: 'First pane' },
+  { title: 'two', content: 'Second pane' },
+  { title: 'three', content: 'Third pane' }
+];
 
 function Root() {
   return (
     <div>
       <Clock />
+      <div className='interactive'>
+        <Tabs panes={panes} />
+      </div>
     </div>
   );
 }
+
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(<Root />, document.getElementById('main'));
