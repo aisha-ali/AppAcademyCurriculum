d1d8f5b2ce0a1e9ce9d97c0ff5859e73e46e79e0
aisha-ali <aali.aishaali@gmail.com>
2021-02-01 17:56:13 -0500

    store

---
+++ React/React-ReduxTodos/frontend/reducers/todos_reducer.js
@@ -0,0 +1,11 @@
+const todosReducer = (state = {}, action) => {
+  Object.freeze(state);
+  let nextState = {};
+
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default todosReducer;
