7d8d0b455764dcee5116c0d7d7113db275691fe4
aisha-ali <aali.aishaali@gmail.com>
2021-02-04 19:47:48 -0500

    args

---
+++ React/learningmiddleware+thunks/arg.js
@@ -0,0 +1,55 @@
+function foo(arg) {
+  return arg;
+}
+
+foo(5); // => 5
+foo(); // => undefined
+
+
+function logArguments(arg1, arg2) {
+  let result = [];
+
+  for (let i = 0; i < arguments.length; i++) {
+    result.push(arguments[i]);
+  }
+
+  return result
+}
+
+logArguments("boop", "candle", 3); // ["boop", "candle", 3]
+
+
+function thisWorks() {
+  let args = Array.prototype.slice.call(arguments);
+  args instanceof Array; //=> true
+  args.forEach((arg) => console.log(arg)); // This works!
+}
+
+function thisWorksToo() {
+  let args = Array.from(arguments);
+  args instanceof Array; //=> true
+  args.forEach((arg) => console.log(arg)); // This works too!
+}
+
+
+function argumentsWay(firstArg) {
+  console.log(`The first arg is ${firstArg}!`);
+
+  // We grab the arguments and call slice with 1 to eliminate the firstArg
+  let otherArgs = Array.prototype.slice.call(arguments, 1);
+  console.log(`The other args are:`);
+
+  otherArgs.forEach((arg) => {
+    console.log(arg);
+  });
+}
+
+function restWay(firstArg, ...otherArgs) {
+  console.log(`The first arg is ${firstArg}!`);
+
+  console.log(`The other args are:`);
+
+  otherArgs.forEach((arg) => {
+    console.log(arg);
+  });
+}
