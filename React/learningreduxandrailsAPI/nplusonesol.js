1b08f34fd0c91ad04b177b6f974d2e2369ffb618
aisha-ali <aali.aishaali@gmail.com>
2021-02-04 21:04:05 -0500

    complex includes:

--- React/learningreduxandrailsAPI/nplusonesol.js
+++ React/learningreduxandrailsAPI/nplusonesol.js
@@ -9,3 +9,10 @@ class User < ApplicationRecord
     post_comment_counts
   end
 end
+
+
+comments = user.comments.includes(: post, : parent_comment)
+
+
+posts = user.posts.includes(: comments => [: author, : parent_comment])
+first_post = posts[0]
