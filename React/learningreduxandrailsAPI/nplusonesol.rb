00fe82aee64095f565bce8c71cd7d213e3af3ffc
aisha-ali <aali.aishaali@gmail.com>
2021-02-04 21:05:26 -0500

    outer joins

--- React/learningreduxandrailsAPI/nplusonesol.rb
+++ React/learningreduxandrailsAPI/nplusonesol.rb
@@ -16,3 +16,12 @@ comments = user.comments.includes(: post, : parent_comment)
 
 posts = user.posts.includes(: comments => [: author, : parent_comment])
 first_post = posts[0]
+
+
+// outer joins
+
+posts_with_counts = self
+  .posts
+  .select('posts.*, COUNT(comments.id) AS comments_count')
+  .left_outer_joins(: comments)
+    .group('posts.id')
