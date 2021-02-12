260e686686a43e79ea6669907f0a3f03a68260f5
aisha-ali <aali.aishaali@gmail.com>
2021-02-11 23:12:40 -0500

    learning withRouter

---
+++ React/learningFrontendRoutingWithReact/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Examplee extends React.Component {
+
+  const Header = ({ match }) => (
+    // ...
+    User id: { match.params.userId
+}
+)
+
+export default withRouter(Header)
