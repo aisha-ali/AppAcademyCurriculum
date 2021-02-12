83cedf3cd0459db14910ea30939c52667df6f42f
aisha-ali <aali.aishaali@gmail.com>
2021-02-11 23:20:57 -0500

    question2

---
+++ React/learningFrontendRoutingWithReact/q2.jsx
@@ -0,0 +1,9 @@
+<HashRouter>
+  <div>
+    <Route exact path="/" component={App} />
+    <Route path="/profile" component={Profile} />
+    <Route path="/album" component={Album} />
+  </div>
+</HashRouter>
+
+// Profile
