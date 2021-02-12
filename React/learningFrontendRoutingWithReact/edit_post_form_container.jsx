9a3813feb3c74e7b946ecd6197dd35314f5b0757
aisha-ali <aali.aishaali@gmail.com>
2021-02-11 23:17:55 -0500

    edit post form

--- React/learningFrontendRoutingWithReact/edit_post_form_container.jsx
+++ React/learningFrontendRoutingWithReact/edit_post_form_container.jsx
@@ -3,3 +3,15 @@ import React from "react";
 import PostForm from "./post_form";
 import { fetchPost, updatePost } from "../actions/post_actions";
 import { selectPost } from "../selectors";
+
+const mapStateToProps = (state, ownProps) => {
+  const defaultPost = { title: "", body: "" };
+  const post = selectPost(ownProps.match.params.postId) || defaultPost;
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchPost: id => dispatch(fetchPost(id)),
+    submit: post => dispatch(updatePost(post))
+  };
+};
