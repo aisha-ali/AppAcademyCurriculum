87fb38e1359f8d421e49c82a73f77a9435c4841a
aisha-ali <aali.aishaali@gmail.com>
2021-02-11 23:09:55 -0500

    learning link comp

---
+++ React/learningFrontendRoutingWithReact/app.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Example extends React.Component {
+
+
+<NavLink to={`users/${user.id}`}
+  activeClassName="active">{user.name}</NavLink> 
+
+
+<a href="#/users/123" class="active">Michael</a>
+
+<a href="#/users/123">Michael</a>
+
+
+<NavLink to={`users/${user.id}`}
+  activeStyle={{ fontWeight: 'bold' }}>{user.name}</NavLink> 
+
+<a href="#/users/123" style="font-weight:bold;"
+  class="active">Michael</a> 
+
+<a href="#/users/123">Michael</a>
+
+}
