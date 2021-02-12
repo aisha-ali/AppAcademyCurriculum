955568504fe17f0f98de122dba22d850c1228c73
aisha-ali <aali.aishaali@gmail.com>
2021-02-11 23:21:22 -0500

    question3

---
+++ React/learningFrontendRoutingWithReact/q3.jsx
@@ -0,0 +1,11 @@
+<HashRouter>
+  <div>
+    <Switch>
+      <Route path="/" component={App} />
+      <Route path="/profile" component={Profile} />
+      <Route path="/album" component={Album} />
+    </Switch>
+  </div>
+</HashRouter>
+
+// App
