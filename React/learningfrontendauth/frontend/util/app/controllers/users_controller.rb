6640c8e6a0613563f2d11b9a79bdb70dd2cd08d2
aisha-ali <aali.aishaali@gmail.com>
2021-02-12 23:56:07 -0500

    controller2

--- React/learningfrontendauth/frontend/util/app/controllers/users_controller.rb
+++ React/learningfrontendauth/frontend/util/app/controllers/users_controller.rb
@@ -8,3 +8,14 @@ class UsersController < ApplicationController
       render json: @user.errors.full_messages
     end
   end
+
+  def new
+    @user = User.new
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
