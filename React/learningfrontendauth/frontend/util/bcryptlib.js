ca81caf8b0ca84984a538710f2b806e654a99054
aisha-ali <aali.aishaali@gmail.com>
2021-02-12 23:53:37 -0500

    bcrypt

--- React/learningfrontendauth/frontend/util/bcryptlib.js
+++ React/learningfrontendauth/frontend/util/bcryptlib.js
@@ -3,3 +3,10 @@ require 'bcrypt'
 password_hash = BCrypt:: Password.create('my_secret_password')
 password_hash.is_password ? ('my_secret_password')
 password_hash.is_password ? ('not_my_secret_password')
+
+
+class User<ApplicationRecord
+  def password = (password)
+    self.password_digest = BCrypt:: Password.create(password)
+end
+end
