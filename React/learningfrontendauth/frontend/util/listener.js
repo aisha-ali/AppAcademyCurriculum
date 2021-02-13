6d516f95b4cd14c00b447e929d8b3239afcc09d4
aisha-ali <aali.aishaali@gmail.com>
2021-02-12 23:50:48 -0500

    listener

---
+++ React/learningfrontendauth/frontend/util/listener.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let store;
+  if (window.currentUser) {
+    const { currentUser } = window;
+    const { id } = currentUser;
+    const preloadedState = {
+      entitities: {
+        users: {
+          [id]: currentUser
+        }
+      }
+      session: { id }
+    };
+    store = configureStore(preloadedState);
+
+    // Clean up after ourselves so we don't accidentally use the
+    // global currentUser instead of the one in the store
+    delete window.currentUser;
+
+  } else {
+    store = configureStore();
+  }
