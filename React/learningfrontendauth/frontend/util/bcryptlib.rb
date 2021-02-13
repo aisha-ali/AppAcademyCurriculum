868b637c1cfbae572ad50b328a89582bc60f84f8
aisha-ali <aali.aishaali@gmail.com>
2021-02-12 23:55:05 -0500

    longer way

--- React/learningfrontendauth/frontend/util/bcryptlib.rb
+++ React/learningfrontendauth/frontend/util/bcryptlib.rb
@@ -5,8 +5,19 @@ password_hash.is_password ? ('my_secret_password')
 password_hash.is_password ? ('not_my_secret_password')
 
 
-class User<ApplicationRecord
+class User < ApplicationRecord
   def password = (password)
     self.password_digest = BCrypt:: Password.create(password)
+  end
 end
+
+
+class User < ApplicationRecord
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
 end
