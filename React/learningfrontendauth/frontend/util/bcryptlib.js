7921f320a3e0f4be70131eff57c994855af76c6c
aisha-ali <aali.aishaali@gmail.com>
2021-02-12 23:52:41 -0500

    bcrypt review

---
+++ React/learningfrontendauth/frontend/util/bcryptlib.js
@@ -0,0 +1,5 @@
+require 'bcrypt'
+
+password_hash = BCrypt:: Password.create('my_secret_password')
+password_hash.is_password ? ('my_secret_password')
+password_hash.is_password ? ('not_my_secret_password')
