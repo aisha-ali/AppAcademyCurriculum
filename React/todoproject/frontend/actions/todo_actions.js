e7e54bfe597c0560caa970f6ec4f0cdf91e4c260
aisha-ali <aali.aishaali@gmail.com>
2021-02-14 17:58:42 -0500

    root reducer

--- React/todoproject/frontend/actions/todo_actions.js
+++ React/todoproject/frontend/actions/todo_actions.js
@@ -2,6 +2,7 @@ export const RECEIVE_TODOS = 'RECEIVE_TODOS';
 export const RECEIVE_TODO = 'RECEIVE_TODO';
 export const REMOVE_TODO = 'REMOVE_TODO';
 export const TODO_ERROR = 'TODO_ERROR';
+
 export const receiveTodos = todos => ({
   type: RECEIVE_TODOS,
   todos,
---
+++ React/todoproject/frontend/reducers/todos_reducer.js
@@ -0,0 +1,28 @@
+import {
+  RECEIVE_TODOS,
+  RECEIVE_TODO,
+  REMOVE_TODO,
+  TODO_ERROR
+} from '../actions/todo_actions';
+
+const todosReducer = (state = {}, action) => {
+    default:
+      return state;
+  }
+};
+export default todosReducer;
+// Sample State Shape
+// {
+// "1": {
+// id: 1,
+// title: "wash car",
+// body: "with soap",
+// done: false
+// },
+// "2": {
+// id: 2,
+// title: "wash dog",
+// body: "with shampoo",
+// done: true
+// },
+// };
