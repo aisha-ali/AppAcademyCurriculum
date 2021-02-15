ac87d3625857e3e492eb8e235ad784e6e0c595fe
aisha-ali <aali.aishaali@gmail.com>
2021-02-15 18:25:58 -0500

    step listt

--- React/todoproject/frontend/components/step_list/step_list_container.jsx
+++ React/todoproject/frontend/components/step_list/step_list_container.jsx
@@ -3,6 +3,7 @@ import StepList from './step_list';
 // Actions
 import { stepsByTodoId } from '../../reducers/selectors';
 import { receiveStep } from '../../actions/step_actions';
+
 const mapStateToProps = (state, { todo_id }) => ({
   steps: stepsByTodoId(state, todo_id),
   todo_id
