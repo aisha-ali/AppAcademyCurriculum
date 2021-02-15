c146d0a87e95da063b09e2f3ca43f0951462682e
aisha-ali <aali.aishaali@gmail.com>
2021-02-14 20:42:41 -0500

    app

--- React/todoproject/frontend/todo_redux.jsx
+++ React/todoproject/frontend/todo_redux.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 
-
-
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
@@ -12,4 +10,3 @@ document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
 });
-
