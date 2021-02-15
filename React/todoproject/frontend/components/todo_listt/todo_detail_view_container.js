8c77179dee17b461133e6c4866d691a4dc99d6bf
aisha-ali <aali.aishaali@gmail.com>
2021-02-15 16:23:56 -0500

    step list

--- React/todoproject/frontend/components/todo_listt/todo_detail_view_container.js
+++ React/todoproject/frontend/components/todo_listt/todo_detail_view_container.js
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux';
-import TodoDetailView from './todo_detail_view';
+import TodoDetailView from './todo_detail_view_container';
 // Actions
 import { removeTodo } from '../../actions/todo_actions';
 import { receiveSteps } from '../../actions/step_actions';
--- React/todoproject/frontend/reducers/selectors.js
+++ React/todoproject/frontend/reducers/selectors.js
@@ -1,4 +1,6 @@
 export const allTodos = ({ todos }) => Object.keys(todos).map(id => todos[id]);
+
+
 export const stepsByTodoId = ({ steps }, todo_id) => {
   const stepsByTodoId = [];
   Object.keys(steps).forEach(stepId => {
