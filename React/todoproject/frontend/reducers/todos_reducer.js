94d676aa4bd2f61dbcf7194690f0e3e8f357220e
aisha-ali <aali.aishaali@gmail.com>
2021-02-14 19:19:42 -0500

    store

--- React/todoproject/frontend/reducers/todos_reducer.js
+++ React/todoproject/frontend/reducers/todos_reducer.js
@@ -5,24 +5,11 @@ import {
   TODO_ERROR
 } from '../actions/todo_actions';
 
+
 const todosReducer = (state = {}, action) => {
-    default:
-      return state;
+    switch (action.types) {
+      default:
+        return state;
   }
 };
 export default todosReducer;
-// Sample State Shape
-// {
-// "1": {
-// id: 1,
-// title: "wash car",
-// body: "with soap",
-// done: false
-// },
-// "2": {
-// id: 2,
-// title: "wash dog",
-// body: "with shampoo",
-// done: true
-// },
-// };
