ab565e68a7926453b22a2fa6678e78c616718981
aisha-ali <aali.aishaali@gmail.com>
2021-02-14 19:37:53 -0500

    reducers

--- React/todoproject/frontend/reducers/todos_reducer.js
+++ React/todoproject/frontend/reducers/todos_reducer.js
@@ -5,11 +5,26 @@ import {
   TODO_ERROR
 } from '../actions/todo_actions';
 
-
 const todosReducer = (state = {}, action) => {
-    switch (action.types) {
-      default:
-        return state;
+  Object.freeze(state);
+  let nextState = {};
+  switch (action.type) {
+    case RECEIVE_TODOS:
+      action.todos.forEach(todo => {
+        nextState[todo.id] = todo;
+      });
+      return nextState;
+    case RECEIVE_TODO:
+      const newTodo = { [action.todo.id]: action.todo };
+      return Object.assign({}, state, newTodo);
+    case REMOVE_TODO:
+      nextState = Object.assign({}, state);
+      delete nextState[action.todo.id];
+      return nextState;
+    case TODO_ERROR:
+      alert(action.error);
+    default:
+      return state;
   }
 };
 export default todosReducer;
