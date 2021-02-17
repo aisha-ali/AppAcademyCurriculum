bc785af31bf91e6049f1a8ce39885764362076ee
aisha-ali <aali.aishaali@gmail.com>
2021-02-17 17:58:32 -0500

    migration

---
+++ React/todoproject/mytodoapp/app/controllers/api/todos_controller.rb
@@ -0,0 +1,12 @@
+def show
+  render json: Todo.find(params[:id])
+end
+
+def create
+  @todo = Todo.new(todo_params)
+  if @todo.save
+    render json: @todo
+  else
+    render json: @todo.errors.full_messages, status: 422
+  end
+end
