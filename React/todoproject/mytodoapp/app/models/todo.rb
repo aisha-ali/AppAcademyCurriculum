97b54cc9b8a9f3be285fd6617fd26f956fd20371
aisha-ali <aali.aishaali@gmail.com>
2021-02-17 22:32:53 -0500

    ex

--- React/todoproject/mytodoapp/app/models/todo.rb
+++ React/todoproject/mytodoapp/app/models/todo.rb
@@ -2,3 +2,10 @@ class Todo < ApplicationRecord
 validates :title, :body, presence: true
 validates :done, inclusion: { in: [true, false] }
 end
+
+# == Schema Information
+#
+# Table name: todos
+#
+# id :integer not null, primary key
+# title :string not null
