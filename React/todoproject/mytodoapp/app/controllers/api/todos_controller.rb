b156d9e989c9666e110310fe8d1fa0a07428c21d
aisha-ali <aali.aishaali@gmail.com>
2021-02-17 22:33:47 -0500

    correction

--- React/todoproject/mytodoapp/app/controllers/api/todos_controller.rb
+++ React/todoproject/mytodoapp/app/controllers/api/todos_controller.rb
@@ -16,26 +16,3 @@ before_action :deny_access_if_not_logged_in
       render json: @todo.errors.full_messages, status: 422
     end
   end
-
-  def destroy
-    @todo = current_user.todos.find(params[:id])
-    @todo.destroy
-    render json: @todo, include: :tags
-  end
-
-  def update
-    @todo = Todo.find(params[:id])
-    if @todo.update(todo_params)
-      render json: @todo, include: :tags
-    else
-      render json: @todo.errors.full_messages, status: 422
-    end
-  end
-
-  private
-  def todo_params
-    params.require(:todo).permit(:title, :body, :done, tag_names: [])
-  end
-end
-
-
