a5db0a931fefc143b8e6356cd4895f98beebf6e8
aisha-ali <aali.aishaali@gmail.com>
2021-02-21 16:55:37 -0500

    correction

--- React/skeleton 5/components/giphys_search_container.jsx
+++ React/skeleton 5/components/giphys_search_container.jsx
@@ -2,9 +2,6 @@ import { connect } from 'react-redux';
 import GiphysSearch from './giphys_search';
 import { fetchSearchGiphys } from '../actions/giphy_actions';
 
-import { connect } from 'react-redux';
-import GiphysSearch from './giphys_search';
-import { fetchSearchGiphys } from '../actions/giphy_actions';
 const mapStateToProps = state => {
   return { giphys: state.giphys };
 };
