3ff2d614d4ee5bf9e10f4dff4de6014c8a99a2fa
aisha-ali <aali.aishaali@gmail.com>
2021-02-27 16:49:45 -0500

    addd

--- linked_list_project/lib/linked_list.js
+++ linked_list_project/lib/linked_list.js
@@ -63,8 +63,15 @@ class LinkedList {
         }
         return current;
     }
-
-    
-
-    
-    
+    addToHead(val) {
+        let newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
