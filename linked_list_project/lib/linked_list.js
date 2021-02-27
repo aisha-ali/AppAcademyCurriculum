d2ec3429017b87afa6056940e980ef2d8ce9f4c1
aisha-ali <aali.aishaali@gmail.com>
2021-02-27 16:15:11 -0500

    initial commit

---
+++ linked_list_project/lib/linked_list.js
@@ -0,0 +1,24 @@
+// ============================================================================
+// Implementation Exercise: Singly Linked List
+// ============================================================================
+//
+// -------
+// Prompt:
+// -------
+//
+// Implement a Singly Linked List and all of its methods below!
+//
+// ------------
+// Constraints:
+// ------------
+//
+// Make sure the time and space complexity of each is equivalent to those 
+// in the table provided in the Time and Space Complexity Analysis section
+// of your Linked List reading!
+//
+// -----------
+// Let's Code!
+// -----------
+
+class Node {
+    
