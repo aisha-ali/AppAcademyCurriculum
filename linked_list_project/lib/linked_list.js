ef7d38a74088d701e6692288921c4669150f4bad
aisha-ali <aali.aishaali@gmail.com>
2021-02-27 16:50:43 -0500

    get index

--- linked_list_project/lib/linked_list.js
+++ linked_list_project/lib/linked_list.js
@@ -96,6 +96,13 @@ class LinkedList {
         return false;
     }
 
-   
-
-
+    get(index) {
+        if (index < 0 || index >= this.length) return null;
+        let counter = 0;
+        let current = this.head;
+        while (counter !== index) {
+            current = current.next;
+            counter++;
+        }
+        return current;
+    }
