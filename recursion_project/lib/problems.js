b4091c41bbfdf023c80f7fa17af7995f5009072a
aisha-ali <aali.aishaali@gmail.com>
2021-05-03 14:45:43 -0400

    rec prob

--- recursion_project/lib/problems.js
+++ recursion_project/lib/problems.js
@@ -162,3 +162,52 @@ function pow(base, exponent) {
 //     1-dimensional array: ['some data']
 //     2-dimensional array: [['some data']]
 //     3-dimensional array: [[['some data']]]
+function flatten(data) {
+    if (!Array.isArray(data)) return [data];
+
+    const flat = [];
+    data.forEach((el) => {
+        flat.push(...flatten(el));
+    });
+    return flat;
+}
+
+// Write a function, fileFinder(directories, targetFile), that accepts an object representing directories and a string respresenting a filename.
+// The function should return true, if the file is contained anywhere in the given directories.
+// Note that directory names will begin with '/', but file names will not.
+// 
+// Example:
+//
+// let desktop = {
+//     '/images': {
+//         'app_academy_logo.svg': null,
+//         '/parks': {
+//             'yosemite.jpeg': null,
+//             'acadia.jpeg': null,
+//             'yellowstone.png': null
+//         },
+//         '/pets': {
+//             'trixie_lou.jpeg': null,
+//             'rolo.jpeg': null,
+//             'opal.jpeg': null,
+//             'diana.jpeg': null,
+//         }
+//     },
+//     '/music': {
+//         'hey_programmers.mp3': null,
+//         '/genres': {
+//             '/rock': {
+//                 'everlong.flac': null,
+//                 'livin_on_a_prayer.mp3': null
+//             },
+//             '/hip_hop': {
+//                 'express_yourself.wav': null,
+//                 'ny_state_of_mind.mp3': null
+//             }
+//         }
+//     }
+// };
+//
+// fileFinder(desktop, 'app_academy_logo.svg');     // => true
+// fileFinder(desktop, 'everlong.flac');            // => true
+// fileFinder(desktop, 'sequoia.jpeg');             // => false
